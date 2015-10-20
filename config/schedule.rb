93cbd5c44871b5e44e0a8ef27161250786d87573
Yu Wang <wangyuhere@gmail.com>
2015-10-20 20:22:25 +0000

    Log cron jobs

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,12 +1,12 @@
 every 1.day, at: "1am" do
-  rake "update"
+  rake "update", output: "./log/update.log"
 end
 
 every 1.day, at: "2am" do
-  rake "run"
+  rake "run", output: "./log/run.log"
 end
 
 every 1.day, at: "3am" do
-  rake "deploy"
+  rake "deploy", output: "./log/deploy.log"
 end
 
